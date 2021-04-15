c4b583503f7d2dc7b6fa2194220f970006f6a784
Tirmex <57995718+Tirmex@users.noreply.github.com>
2021-04-15 19:26:29 +0300

    Add files via upload

--- js/my_slider.js
+++ js/my_slider.js
@@ -1,17 +1,43 @@
-const slides_one = document.querySelectorAll("#sl_one .slide_item");
-const slides_two = document.querySelectorAll("#sl_two .slide_item");
 
-var i = 0;
-function change () {
-    slides_one[i].classList.remove("active_slide");
-    slides_two[i].classList.remove("active_slide");
-    i++;
-    if (i == 3){
-        i = 0;
+    const first_slider = document.getElementById("sl_one").firstElementChild.children;
+    
+    const second_slider = document.getElementById("sl_two").firstElementChild.children;
+
+    // var fr_sl_num = {number: 0 };
+    // var sc_sl_num = {number: 0 };
+
+    var active_slide = document.querySelector(".active_slide");
+
+    function change_slide(obj) {
+        clearTimeout(timerid);
+        obj.classList.remove("active_slide");
+        if ( obj.nextElementSibling == null ){
+            first_slider[0].classList.add("active_slide");
+        }
+        else {
+            obj.nextElementSibling.classList.add("active_slide");
+        }
+        active_slide = document.querySelector(".active_slide");
+        timerid = setTimeout(change_slide, 4000, active_slide);
     }
-    slides_one[i].classList.add("active_slide");
-    slides_two[i].classList.add("active_slide");
-}
 
-setInterval(change, 4000);
+    function focus(number){
+        clearTimeout(timerid);
+        for(var i = 0; i<3 ;i++){
+            first_slider[i].classList.remove("active_slide");
+        }
+        first_slider[number].classList.add("active_slide");
+        active_slide = document.querySelector(".active_slide");
+        timerid = setTimeout(change_slide, 4000, active_slide);
+    }
 
+    // навигация
+    const first_slider_nav = document.getElementById("sl_one").lastElementChild.children;
+    const second_slider_nav = document.getElementById("sl_two").lastElementChild.children;
+
+    first_slider_nav[0].addEventListener("click", () => focus(0));
+    first_slider_nav[1].addEventListener("click", () => focus(1));
+    first_slider_nav[2].addEventListener("click", () => focus(2));
+
+    timerid = setTimeout(change_slide, 4000, active_slide);
+    // setInterval(change_slide, 4000, second_slider, sc_sl_num);
